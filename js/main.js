32ad029d44ca77ce6fc1309297890a8588fdf7a6
nadaagomaa <nada.swidaan@gmail.com>
2025-03-06 12:23:33 +0200

    modify html files

--- js/main.js
+++ js/main.js
@@ -2,18 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const navbarToggler = document.querySelector('.navbar-toggler');
     const navbarCollapse = document.querySelector('.navbar-collapse');
     const CloseMenuBtn = document.getElementById('closeMenuBtn');
-    const overlayBackdrop = document.querySelector('.overlay-backdrop');
-  
+    const overlayBackdrop = document.querySelector('.overlay-backdrop'); 
     function toggleMenu() {
       navbarCollapse.classList.toggle('show');
       overlayBackdrop.classList.toggle('show');
-    }
-  
+    } 
     navbarToggler.addEventListener('click', function(e) {
       e.stopPropagation();
       toggleMenu();
     });
-  
     CloseMenuBtn.addEventListener('click', function() {
       toggleMenu();
     });});
