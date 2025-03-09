919ebd255f60432cb276a7ae754bcd95bccbf800
nadaagomaa <nada.swidaan@gmail.com>
2025-03-09 14:17:46 +0200

    modify html, scss, js files

--- js/main.js
+++ js/main.js
@@ -1,16 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
+
+  document.addEventListener('DOMContentLoaded', function() {
     const navbarToggler = document.querySelector('.navbar-toggler');
     const navbarCollapse = document.querySelector('.navbar-collapse');
     const CloseMenuBtn = document.getElementById('closeMenuBtn');
     const overlayBackdrop = document.querySelector('.overlay-backdrop'); 
+    const body = document.body;
     function toggleMenu() {
       navbarCollapse.classList.toggle('show');
       overlayBackdrop.classList.toggle('show');
-    } 
+      body.classList.toggle('overflow-hidden');
+    }
     navbarToggler.addEventListener('click', function(e) {
       e.stopPropagation();
       toggleMenu();
     });
     CloseMenuBtn.addEventListener('click', function() {
       toggleMenu();
-    });});
+    });
+    // Function to close the navbar on resize if it's open
+    function closeNavbarOnResize() {
+      if (window.innerWidth  && navbarCollapse.classList.contains('show')) {
+        navbarCollapse.classList.remove('show');
+        overlayBackdrop.classList.remove('show');
+      }
+    }
+    // Debounce function
+    let resizeTimeout;
+    function debouncedCloseNavbarOnResize() {
+      clearTimeout(resizeTimeout);
+      resizeTimeout = setTimeout(closeNavbarOnResize, 50); 
+    }
+    // Add event listener for window resize with debouncing
+    window.addEventListener('resize', debouncedCloseNavbarOnResize); 
+  });
+ 
+ 
